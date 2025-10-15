8f5b9a0588b6cb8d8de595a68b41cd8267e2bebf
Pdwxn <pdwxn@protonmail.com>
2025-10-15 13:00:40 -0300

    styling cart

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,13 +1,26 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-function Button({ children, disabled, to }) {
-  const className =
-    "inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4";
-  if (to) return <Link to={to} className={className}>{children}</Link>;
+function Button({ children, disabled, to, type }) {
+  const base =
+    "inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
+
+  const styles = {
+    primary: base + " px-4 py-3 md:px-6 md:py-4",
+    small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    secondary:
+      "inline-block rounded-full bg-yellow-400 font-semibold uppercase border-2 border-stone-300 tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-800 focus:bg-stone-300 hover:text-stone-800 focus:outline-none focus:ring focus:text-stone-800 focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed",
+  };
+
+  if (to)
+    return (
+      <Link to={to} className={styles[type]}>
+        {children}
+      </Link>
+    );
 
   return (
-    <button disabled={disabled} className={className}>
+    <button disabled={disabled} className={styles[type]}>
       {children}
     </button>
   );
@@ -17,6 +30,7 @@ Button.propTypes = {
   children: PropTypes.node.isRequired,
   disabled: PropTypes.bool,
   to: PropTypes.string,
+  type: PropTypes.string,
 };
 
 export default Button;
