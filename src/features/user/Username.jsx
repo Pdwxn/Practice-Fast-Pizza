d21bd4d1e69cf928250fb577ea221404164ed19c
Pdwxn <pdwxn@protonmail.com>
2025-10-15 12:04:26 -0300

    styling v1

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <div className="hidden text-sm font-semibold md:block">Joe Doe</div>;
+}
+
+export default Username;
--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,5 +1,9 @@
 function Loader() {
-  return <div className="loader"></div>;
+  return (
+    <section className="absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm">
+      <div className="loader"></div>
+    </section>
+  );
 }
 
 export default Loader;
